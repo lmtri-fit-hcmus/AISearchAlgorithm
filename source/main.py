70de3243ca32071697252f40dfee08b80fd5ce3e
unknown <lengocduc195khtn@gmail.com>
2022-10-19 14:33:01 +0700

    Update divide files

--- source/main.py
+++ source/main.py
@@ -10,11 +10,11 @@ def SearchAlgorithmVisual():
     ROWS = len(matrix)
     COLS = len(matrix[0])
 
-    WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
-    width = WIDTH
-    draw(WIN, grid, ROWS, width,bonus_points)
-    Astar(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]],matrix, nonBonusPointAstarHFunct1)
-    createVideo("4.mp4")
+    # WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
+    # width = WIDTH
+    # draw(WIN, grid, ROWS, width,bonus_points)
+    # Astar(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]],matrix, nonBonusPointAstarHFunct1)
+    # createVideo("4.mp4")
 
     # WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
     # width = WIDTH
@@ -22,11 +22,11 @@ def SearchAlgorithmVisual():
     # Astar(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]],matrix, nonBonusPointAstarHFunct2)
     # createVideo("5.mp4")
 
-    # WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
-    # width = WIDTH
-    # draw(WIN, grid, ROWS, width,bonus_points)
-    # DFS(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]])   #having lambda lets you run the function inside the function
-    # createVideo("1.mp4")
+    WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
+    width = WIDTH
+    draw(WIN, grid, ROWS, width,bonus_points)
+    DFS(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]])   #having lambda lets you run the function inside the function
+    createVideo("1.mp4")
     
     # WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
     # width = WIDTH
