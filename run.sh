cbcc2b85b051f1364dc044f4c067cbdbc5c35332
Minh-Tri Le (lambert) <82067663+lmtri-fit-hcmus@users.noreply.github.com>
2022-10-22 10:11:59 +0700

    add bash file

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Initialize python library
+pip install moviepy.editor
+pip install pygame
+pip install numpy
+
+python main.py
--- source/ai_search_createvideo.py
+++ source/ai_search_createvideo.py
@@ -1,4 +1,3 @@
-from more_itertools import only
 from moviepy.editor import *
 from os import listdir
 from os.path import isfile, join
