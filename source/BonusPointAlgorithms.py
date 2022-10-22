a350d86e8b1d4eaea3af829d125d91e91fc3c2c3
Minh-Tri Le (lambert) <82067663+lmtri-fit-hcmus@users.noreply.github.com>
2022-10-22 17:14:22 +0700

    .

--- source/BonusPointAlgorithms.py
+++ source/BonusPointAlgorithms.py
@@ -1,4 +1,5 @@
 from operator import ne
+import queue
 from time import sleep
 from ai_search_helper import *
 from ai_search_visualization import *
@@ -9,26 +10,14 @@ from queue import Queue, PriorityQueue
 import sys
 import pygame.camera
 
-def heuristic_bonus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus_point):
+def heuristic_bonus_point(win, draw, grid, start: Spot, exit: Spot, H, bonus_point):
     currentPoint=Spot(bonus_point[1], bonus_point[0], exit.width, exit.row,exit.col)
     return H(start, currentPoint) + H(currentPoint,exit) + bonus_point[2]
 
-def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus_points):
+def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, H, bonus_points):
     priorQ = PriorityQueue()
     priorQ.put((0, (start, [start])))
     
-    
-    # pathPoint = PriorityQueue()
-    # pathPoint.put((0,((exit.x, exit.y, -10000), [(exit.x, exit.y, -10000)])))
-    
-    # listBonusPoints=[]
-    # for i in range(0, len(bonus_points)):
-    #     cur = Spot(bonus_points[i][0], bonus_points[i][1], exit.width, exit.row, exit.col)
-    #     listBonusPoints.append(cur)
-    
-    ### Tạm thời
-    # target = (exit.x, exit.y, -10000)
-    ###
     
     visited = []
     costSoFar:dict[Spot, int] = {}
@@ -48,9 +37,9 @@ def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus
         
         # Kiểm tra nếu điểm đang xét là một điểm cộng
         # thì chúng ta thực hiện giảm độ dài đường đi tương ứng với điểm cộng đó
-        print("Choose "+str(currentVertex.x/currentVertex.width)+", "+str(currentVertex.y/currentVertex.width)+" "+str(dis))
-
-        currentVertex.make_closed()
+        # print("Choose "+str(currentVertex.x/currentVertex.width)+", "+str(currentVertex.y/currentVertex.width)+" "+str(dis))
+        if currentVertex != start:
+            currentVertex.make_closed()
         # Đặt giá trị ban đầu của độ dài heuristic dự đoán là số lớn
         # Từ đó đi tìm các độ dài ngắn hơn đến khi tìm được độ dài quãng đường dự đoán là ngắn nhất
 
@@ -79,14 +68,14 @@ def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus
             # Tính hàm h(x_0, exit) = h(x_0, x_bonus) + h(x_bonus, exit) cho biết tổng độ dài khi lựa chọn đi qua điểm thưởng
             # Sau đó kiểm tra chi phí đến điểm nào là tối ưu nhất và đưa dần chúng vào hàng đợi ưu tiên với thứ tự ưu tiên giảm dần 
             # theo dự đoán (thêm cả điểm đích vào hàm đợi ưu tiên đó).
-            print(str(neig.col) + " " + str(neig.row))
-            print("NewCost "+str(newCost))
-            print("Exit cost "+str(heuristicCost))
+            #print(str(neig.col) + " " + str(neig.row))
+            #print("NewCost "+str(newCost))
+            #print("Exit cost "+str(heuristicCost))
 
             for j in range(0,len(bonus_points)):
-                heuristicCurrentCost = heuristic_bonus_point(win, draw, grid, neig , exit, matrix, H, bonus_points[j])
+                heuristicCurrentCost = heuristic_bonus_point(win, draw, grid, neig , exit, H, bonus_points[j])
                 
-                print("heuristic ("+str(bonus_points[j][0])+", "+str(bonus_points[j][1])+ ") "+str(heuristicCurrentCost))
+                #print("heuristic ("+str(bonus_points[j][0])+", "+str(bonus_points[j][1])+ ") "+str(heuristicCurrentCost))
                 # Nếu tìm ra đường đi được dự đoán tốt hơn đi thẳng đến đích thì gán nó là đường đi ưu tiên
                 # Sau bước này, ta sẽ tìm ra đường đi được dự đoán là tốt nhất
                 if(heuristicCurrentCost < heuristicCost):
@@ -96,7 +85,8 @@ def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus
             # Nếu điểm hàng xóm đang xét chưa đi qua, hoặc chi phí mới để đạt đến điểm hiện tại nhỏ hơn chi phí
             # đã tính trước đây thì thực hiện cập nhật chi phí mới
             if neig not in visited or newCost < costSoFar[neig]:
-                neig.make_open()
+                if neig not in priorQ.queue:
+                    neig.make_open()
                 costSoFar[neig] = newCost
                 
                 priority = heuristicCost + costSoFar[neig]
@@ -111,5 +101,76 @@ def Astar_bunus_point(win, draw, grid, start: Spot, exit: Spot, matrix, H, bonus
         print("________")
 
     if not priorQ.empty():
+        reconstruct_path(win, path, draw)
+    return []
+
+
+def GBFS_bunus_point(win, draw, grid, start: Spot, exit: Spot, H, bonus_points):
+    priorQ = PriorityQueue()
+    priorQ.put((0, (start, [start])))
+    visited = []
+    count = 0
+    
+    while not priorQ.empty():
+        
+        # Lấy thông tin điểm có độ ưu tiên cao nhất ra khỏi hàng đợi
+        # Điểm này là điểm hàng xóm kế tiếp có đánh giá là cho con đường đi ngắn nhất trong tất
+        # cả các hàng xóm
+        _ , (currentVertex, path) = priorQ.get()
+        
+        # Nếu đã tìm đến điểm kết thúc thì dừng tìm kiếm và chuyển sang bước in đường đi
+        if(currentVertex == exit):
+            break
+        
+        currentVertex.make_closed()
+
+        bonus = 0
+        for neig in currentVertex.neighbours:
+            j = 0
+            n = len(bonus_points)
+            while(j < n):
+                if(currentVertex.x/currentVertex.width == bonus_points[j][0] and currentVertex.y/currentVertex.width == bonus_points[j][1]):
+                    bonus = bonus + bonus_points[j][2]
+                    bonus_points.remove(bonus_points[j])
+                    j = j - 1
+                    n = n - 1
+                j = j + 1
+
+            # Tính hàm f(x) = h(x) cho biết độ dài đường đi dự đoán nếu ta tiếp tục đi từ điểm hiện tại đến đích
+            heuristicCost =  H(neig,exit)
+ 
+            # Xét khoảng cách từ điểm hàng xóm đang xét đến các điểm cộng
+            # Tìm con đường được dự đoán là ngắn nhất như sau:
+            
+            # Tính hàm h(x_0, exit) = h(x_0, x_bonus) + h(x_bonus, exit) cho biết tổng độ dài khi lựa chọn đi qua điểm thưởng
+            # Sau đó kiểm tra chi phí đến điểm nào là tối ưu nhất và đưa dần chúng vào hàng đợi ưu tiên với thứ tự ưu tiên giảm dần 
+            # theo dự đoán (thêm cả điểm đích vào hàm đợi ưu tiên đó).
+
+            for j in range(0,len(bonus_points)):
+                heuristicCurrentCost = heuristic_bonus_point(win, draw, grid, neig , exit, H, bonus_points[j])
+
+                # Nếu tìm ra đường đi được dự đoán tốt hơn đi thẳng đến đích thì gán nó là đường đi ưu tiên
+                # Sau bước này, ta sẽ tìm ra đường đi được dự đoán là tốt nhất
+                if(heuristicCurrentCost < heuristicCost):
+                    heuristicCost = heuristicCurrentCost
+
+ 
+            # Nếu điểm hàng xóm đang xét chưa đi qua, hoặc chi phí mới để đạt đến điểm hiện tại nhỏ hơn chi phí
+            # đã tính trước đây thì thực hiện cập nhật chi phí mới
+            if neig not in visited:
+                neig.make_open()
+                newCost = 0
+                
+                priority = 0 + heuristicCost
+
+                priorQ.put( (priority, (neig, path + [neig] )))
+                visited.append(neig)
+
+            draw()
+            pygame.image.save(win, "tmp_image/" + str(count) + ".png")
+            count+=1
+
+
+    if not priorQ.empty() or currentVertex == exit:
         reconstruct_path(win, path, draw)
     return []
--- source/main.py
+++ source/main.py
@@ -6,20 +6,20 @@ def SearchAlgorithmVisual():
     #init D
 
     #Get matrix, bonus point, start, end
-    # file_name = "./input/level_1/input2.txt"
-    # bonus_points, matrix = read_file(file_name)
-    # ROWS = len(matrix)
-    # COLS = len(matrix[0])
+    file_name = "./input/level_2/input1.txt"
+    bonus_points, matrix = read_file(file_name)
+    ROWS = len(matrix)
+    COLS = len(matrix[0])
     
-    # WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
-    # width = WIDTH
+    WIN, grid, HEIGHT, WIDTH , start, end = restore_pygame(matrix,COLS,ROWS)
+    width = WIDTH
 
-    # draw(WIN, grid, ROWS, width,bonus_points)
-    # print(str(grid[start[0]][start[1]])+" "+str(grid[end[0]][end[1]])+" "+ str(ManhattanDistance(grid[start[0]][start[1]], grid[end[0]][end[1]])))
-    # UCS(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]])
-    # createVideo("6.mp4")
+    draw(WIN, grid, ROWS, width,bonus_points)
+    #print(str(grid[start[0]][start[1]])+" "+str(grid[end[0]][end[1]])+" "+ str(ManhattanDistance(grid[start[0]][start[1]], grid[end[0]][end[1]])))
+    Astar_bunus_point(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]],ManhattanDistance,bonus_points)
+    createVideo("6.mp4")
     
-    nonBonusStart()
+    #nonBonusStart()
 
     pygame.quit()
 
