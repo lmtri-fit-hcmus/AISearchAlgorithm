2bcf7768ff044f6dbda1d749a0a6c903fb42c5c6
unknown <lengocduc195khtn@gmail.com>
2022-10-23 16:29:09 +0700

    update

--- source/main.py
+++ source/main.py
@@ -3,7 +3,7 @@ from ai_search_helper import *
 
 def SearchAlgorithmVisual():
     #init D
-    file_name = LEVEL2_INPUT_PATH + '/' + 'input1.txt'
+    file_name = LEVEL2_INPUT_PATH + '/' + 'input2.txt'
     bonus_points, matrix = read_file(file_name)
     ROWS = len(matrix)
     COLS = len(matrix[0])
@@ -13,8 +13,8 @@ def SearchAlgorithmVisual():
     width = WIDTH
     draw(WIN, grid, ROWS, width,bonus_points)
     resetTmpImage()
-    isPath, length = GBFS_bonus_point(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]], EuclidDistance,bonus_points)   #having lambda lets you run the function inside the function
-    #createVideo(output_path + '/gbfs/gbfs_heuristic_2.mp4') 
+    isPath, length = Astar_bonus_point(WIN, lambda: draw(WIN, grid, ROWS, width, bonus_points), grid, grid[start[0]][start[1]], grid[end[0]][end[1]], ManhattanDistance,bonus_points)   #having lambda lets you run the function inside the function
+    createVideo('gbfs_heuristic_2.mp4') 
     #createText(isPath, length, output_path + '/gbfs/gbfs_heuristic_2.txt') 
     #nonBonusStart()
     #bonusStart()
